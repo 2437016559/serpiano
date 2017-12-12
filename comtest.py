e9dbe49f5ff149a301bcdf819f2c59d07695ed86
xknxknqq <1553730177@qq.com>
2017-12-12 15:54:28 +0800

    add song name and movment control

--- comtest.py
+++ comtest.py
@@ -5,6 +5,12 @@ import time
 print ('hello')
 ports = list(serial.tools.list_ports.comports())
 print (ports)
+for p in ports:
+    print (p[1])
+    if "Arduino" in p[1]:
+	    ser=serial.Serial(port=p[0])
+    else :
+	    print ("No Arduino Device was found connected to the computer")
 
 song1 = ['1','1','5','5','6','6','5','5','4','4','3','3','2','2','1','1']
 song2 = ['1','2','3','1','1','2','3','1','3','4','5','3','4','5']
@@ -16,12 +22,6 @@ print(rows[0:5])
 row=rows[0]
 song1 = row.split(',')
 
-for p in ports:
-    print (p[1])
-    if "Arduino" in p[1]:
-	    ser=serial.Serial(port=p[0])
-    else :
-	    print ("No Arduino Device was found connected to the computer")
 
 #ser=serial.Serial(port='COM4')
 #ser=serial.Serial(port]='/dev/ttymodem542')
@@ -32,6 +32,7 @@ def run():
         print ('select which song do you want to play ? 1,2 q and others for quit')
         action = input("> ")
         if action == "1":
+            print("song name is:")
             for notes in song1:
                 ser.write(notes.encode())
                 print ("send:"+notes)
